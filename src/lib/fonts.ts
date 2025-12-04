51813b5fc5bc250b373908aa344c1b8cea724a33
ZeelJasani <deltapixel8a@gmail.com>
2025-12-04 16:42:34 +0530

    change font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,15 +1,17 @@
 import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
 import {
   // IBM_Plex_Mono as FontMono,
   IBM_Plex_Sans as FontSans,
 } from "next/font/google";
 
-export const fontSans = FontSans({
-  weight: ["400", "500", "600"],
-  display: "swap",
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+// export const fontSans = FontSans({
+//   weight: ["400", "500", "600"],
+//   display: "swap",
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
+export const fontSans = GeistSans;
 
 // export const fontMono = FontMono({
 //   weight: ["400", "500", "600"],
